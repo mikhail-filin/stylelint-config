98949e4ee0f2207f2e509e8f22ee74c9eb2cfd1d
Mikhail Filin <mikhailf.codes@gmail.com>
2023-11-06 17:22:53 +0400

    feat: add order groups for grid/flex/layout properties

--- groups.js
+++ groups.js
@@ -19,6 +19,55 @@ const groups = [
 			'inset-inline-end',
 		],
 	},
+	{
+		groupName: 'Display',
+		properties: ['box-sizing', 'display'],
+	},
+	{
+		groupName: 'Grid',
+		properties: [
+			'grid',
+			'grid-area',
+			'grid-auto-rows',
+			'grid-row',
+			'grid-row-start',
+			'grid-row-end',
+			'grid-auto-columns',
+			'grid-column',
+			'grid-column-start',
+			'grid-column-end',
+			'grid-template',
+			'grid-template-areas',
+			'grid-template-rows',
+			'grid-template-columns',
+		],
+	},
+	{
+		groupName: 'Flex',
+		properties: [
+			'flex',
+			'flex-basis',
+			'flex-direction',
+			'flex-flow',
+			'flex-grow',
+			'flex-shrink',
+			'flex-wrap',
+		],
+	},
+	{
+		groupName: 'Layout',
+		properties: [
+			'align-content',
+			'align-items',
+			'align-self',
+			'justify-content',
+			'justify-items',
+			'justify-self',
+			'gap',
+			'row-gap',
+			'column-gap',
+		],
+	},
 ];
 
 module.exports = groups;
